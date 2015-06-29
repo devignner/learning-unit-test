14dab8511c735d3f7c94dfc80ef30e1248751037
zibersaioros <zibersaioros@gmail.com>
2015-06-29 17:48:16 +0900

    1. 햄크레스트 테스트 메서드 추가.

--- src/test/java/com/devignner/learning/junit/hamcrest/TestHamcrest.java
+++ src/test/java/com/devignner/learning/junit/hamcrest/TestHamcrest.java
@@ -1,7 +1,7 @@
 package com.devignner.learning.junit.hamcrest;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,5 +34,58 @@ public class TestHamcrest {
 		assertThat(intList, instanceOf(List.class));
 	}
 	
+	@Test
+	public void testEqualTo(){
+		
+		//equals 메서드로 판단.
+		
+		List<Integer> testList = new ArrayList<Integer>();
+		for(int i = 0; i < 10; i++){
+			testList.add(i);
+		}
+		
+		assertThat(testList, equalTo(intList));
+	}
 	
+	@Test
+	public void testNot(){
+		//단순 값 비교
+		assertThat(1, is(not(2)));
+		assertThat(1, not(2));
+		
+		//매처와 혼합
+		assertThat(1, not(equalTo(3)));
+		assertThat(1, is(not(equalTo(3))));
+	}
+	
+	@Test
+	public void testAnything(){
+		//항상 매칭되는 매쳐를 반환.
+		assertThat("test",  anything());
+		assertThat(3.14, anything("test always match"));
+		assertThat("test", anything("test always match"));
+	}
+	
+	@Test
+	public void testInstanceOf(){
+		//is(Class<?>) 대신 사용
+		//Class.isInstance 메서드로 판단.
+		assertThat(intList, is(instanceOf(ArrayList.class)));
+		assertThat(intList, is(instanceOf(List.class)));
+		assertThat(intList, instanceOf(ArrayList.class));
+	}
+	
+	
+	@Test
+	public void testIsA(){
+		//is(instanceOf(Class))의 단축버전이다.
+		assertThat((ArrayList)intList, isA(ArrayList.class));
+		assertThat(intList, isA(List.class));
+	}
+	
+	@Test
+	public void testNullValue(){
+		assertThat(intList, not(nullValue()));
+		assertThat(intList, is(not(nullValue())));
+	}
 }
